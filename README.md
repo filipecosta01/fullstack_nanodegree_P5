22b6a33fb6e958205ddaf874095cde42c4c63202
Filipe Costa <filipecosta@filipecosta01-macbook.local>
2017-05-22 12:12:46 -0300

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fullstack_nanodegree_P5
 Project 5 - Udacity Full Stack Nanodegree - Neighborhood Map
+
+## Design Inspiration
+For the main design inspiration, please check [skelly nav side fixed with bootstrap](http://codeply.com/go/RmXlEZfi8z)
--- src/app.js
+++ src/app.js
@@ -3,11 +3,31 @@ import _ from 'lodash';
 import moment from 'moment';
 import ko from 'knockout';
 
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import BootstrapJS from 'bootstrap/dist/js/bootstrap.js';
-import styles from '../styles/main.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../styles/main.css';
 
 import swal from 'sweetalert/dist/sweetalert.min.js';
-import swalCss from 'sweetalert/dist/sweetalert.css';
+import 'sweetalert/dist/sweetalert.css';
 
-import font_awesome from 'font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'),
+        {
+            center: {lat: 40.74135, lng: -73.99802},
+            zoom: 14
+        }
+    );
+}
+
+window.initMap = initMap;
+
+function main() {
+
+}
+
+$( document ).ready( () => main() );
