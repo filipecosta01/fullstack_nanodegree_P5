8794ac5283c4df1a2e95a198e0a9947d2e577add
Filipe Costa <filipecosta@filipecosta01-macbook.local>
2017-05-22 09:33:43 -0300

    Initial configs

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import moment from 'moment';
+import ko from 'knockout';
+
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import BootstrapJS from 'bootstrap/dist/js/bootstrap.js';
+import styles from '../styles/main.css';
+
+import swal from 'sweetalert/dist/sweetalert.min.js';
+import swalCss from 'sweetalert/dist/sweetalert.css';
+
+import font_awesome from 'font-awesome/css/font-awesome.min.css';
